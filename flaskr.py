0a483fd4e66f1c98e647b3e77a202915799e5440
Salvinus01 <gimenez_jr@hotmail.com>
2021-06-18 19:08:18 +0000

    final apressado

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, g, render_template, request
 
 # Configuracoes
 
@@ -16,6 +16,32 @@ app.config.from_object(__name__)
 def connec_db():
     return sqlite3.connect(DATABASE)
 
+@app.before_request
+def before():
+    g.db = connec_db()
+
+@app.teardown_request
+def after(exception):
+    g.db.close()
+
+#@app.route('/')
+#def index():
+#    entradas = [{'titulo': 'O primeiro Post',
+#                'texto': 'texto do post'},
+#                {'titulo': 'Segundo Post',
+#                'texto': 'Texto do segundo post'},
+#                {'titulo': 'terceiro Post',
+#                'texto': 'terceiro post'}
+#                ]
+#
+#    return render_template('index.html', entradas=entradas)
+
 @app.route('/')
 def index():
-    return "<h1>Hellow World</1>"
+    sql = 'SELECT titulo, texto from entradas order by id desc'
+    cur = g.db.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cur.fetchall()]
+    return render_template('index.html', entradas=entradas)
+
+
+    
